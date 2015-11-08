01b1b0f3c06c75514ede270e3982e3b0356a464e
jessicaar <jessica_a.r@hotmail.com>
2015-11-08 15:12:21 +0000

    Update Background.h

--- ConsoleApplication1/Background.h
+++ ConsoleApplication1/Background.h
@@ -1,11 +1,9 @@
-#pragma once
 #ifndef __BACKGROUND__
 #define __BACKGROUND__
 
 #include "StaticObject.h"
 #include "Vector3.h"
 #include "GL/glut.h"
-
 
 class Background : public StaticObject
 {
@@ -15,50 +13,25 @@ public:
 	inline virtual ~Background() {}
 	inline void draw() {
 		for (int i = -10; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (i % 2 == 0) {
-					if (j % 2 == 0) {
-						//glColor3d(0.74902, 0.847059, 1.847059);
-						defineMaterialBlue();
-					} //0.74902 green 0.847059 blue 0.847059
-					else defineMaterialWhite();
-					glPushMatrix();
-					glTranslated(i, j, 0);
-					glScaled(1, 1, 0);
-					glutSolidCube(1);
-					glPopMatrix();
+			for (int j = -9; j < 10; j++) {
+				if ((i + j) % 2) defineMaterialBlue();
+				else defineMaterialWhite();
 
-					glPushMatrix();
-					glTranslated(-i - 2, -j, 0);
-					glScaled(1, 1, 0);
-					glutSolidCube(1);
-					glPopMatrix();
-				}
-				else {
-					if (j % 2 == 1)defineMaterialBlue();
-					else defineMaterialWhite();
-					glPushMatrix();
-					glTranslated(i, j, 0);
-					glScaled(1, 1, 0);
-					glutSolidCube(1);
-					glPopMatrix();
+				glPushMatrix();
+				glTranslated(i, j, 0);
+				glScaled(1, 1, 0);
+				//glutSolidCube(1);
+				drawCube();
+				glPopMatrix();
 
-					glPushMatrix();
-					glTranslated(-i, -j, 0);
-					glScaled(1, 1, 0);
-					glutSolidCube(1);
-					glPopMatrix();
-				}
 			}
-
 		}
-
 	}
 	void defineMaterialBlue() {
 		defineMaterial(0.074902, 0.0847059, 0.1847059, 1.00,	//Ambient
 			0.074902, 0.0847059, 0.1847059, 1.00,	//Diffuse
 			0.074902, 0.0847059, 0.1847059, 1.00,	//Specular
-			0.00, 0.00, 0.00, 1.00,	//Emission
+			0,0,0, 1.00,	//Emission
 			77);					//SHININESS
 		glColor3f(0.74902, 0.847059, 1.847059);
 	}
@@ -67,9 +40,52 @@ public:
 		defineMaterial(0.1, 0.1, 0.1, 1.00,	//Ambient
 			0.1, 0.1, 0.1, 1.00,	//Diffuse
 			0.1, 0.1, 0.1, 1.00,	//Specular
-			0.00, 0.00, 0.00, 1.00,	//Emission
+			0,0,0, 1.00,	//Emission
 			77);					//SHININESS
 		glColor3f(1,1,1);
+	}
+	void drawCube() {
+		glBegin(GL_POLYGON);
+		glVertex3f(-1, 1, 0);
+		glVertex3f(1, 1, 0);
+		glVertex3f(1, -1, 0);
+		glVertex3f(-1, -1, 0);
+		glEnd();
+		//Upper case
+		glBegin(GL_POLYGON);
+		glVertex3f(-1, 1, 1);
+		glVertex3f(1, 1, 1);
+		glVertex3f(1, -1, 1);
+		glVertex3f(-1, -1, 1);
+		glEnd();
+		//left side
+		glBegin(GL_POLYGON);
+		glVertex3f(-1, -1, 1);
+		glVertex3f(1, -1, 1);
+		glVertex3f(1, -1, 0);
+		glVertex3f(-1, -1, 0);
+		glEnd();
+		//right side
+		glBegin(GL_POLYGON);
+		glVertex3f(1, 1, 1);
+		glVertex3f(-1, 1, 1);
+		glVertex3f(-1, 1, 0);
+		glVertex3f(1, 1, 0);
+		glEnd();
+		//back
+		glBegin(GL_POLYGON);
+		glVertex3f(1, 1, 0);
+		glVertex3f(1, 1, 1);
+		glVertex3f(1, -1, 1);
+		glVertex3f(1, -1, 0);
+		glEnd();
+		//front
+		glBegin(GL_POLYGON);
+		glVertex3f(-1, 1, 0);
+		glVertex3f(-1, 1, 1);
+		glVertex3f(-1, -1, 1);
+		glVertex3f(-1, -1, 0);
+		glEnd();
 	}
 };
 
