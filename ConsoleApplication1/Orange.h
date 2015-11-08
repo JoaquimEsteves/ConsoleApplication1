00ece3e68b1adae3e45a9695948d555974689c2d
jessicaar <jessica_a.r@hotmail.com>
2015-11-08 00:09:24 +0000

    Update Orange.h

--- ConsoleApplication1/Orange.h
+++ ConsoleApplication1/Orange.h
@@ -17,48 +17,55 @@ class Orange : public Obstacle {
 	//bool _visible = true;
 	//ACTIVAR DEPTH BUFFER
 public:
-	
+
 	inline  Orange() {
 	}
 	inline  Orange(double x, double y, double z) {
 		setPosition(x, y, z);
-		setSize(.5,.5,.5);
+		setSize(.5, .5, .5);
 		_counter = rand() % 20 + 1;
 	}
 
 	inline virtual ~Orange() {}
-	void setLost(int i)								{ _lost = i;}
-	int getLost()									{ return _lost; }
-	double getCounter()								{ return _counter; }
-	void setCounter(double c)						{ _counter = c; }
-	double getTurnAngle()							{ return _turnAngle; }
-	void setTurnAngle(double turnAngle)				{ _turnAngle = turnAngle; }
+	void setLost(int i) { _lost = i; }
+	int getLost() { return _lost; }
+	double getCounter() { return _counter; }
+	void setCounter(double c) { _counter = c; }
+	double getTurnAngle() { return _turnAngle; }
+	void setTurnAngle(double turnAngle) { _turnAngle = turnAngle; }
 	//void setVisible(bool b)							{ _visible = b; }
 	//bool getVisible(bool b)							{ return _visible; }
 	inline void draw() {
 		//if (_visible) {
-			//orange
-			glPushMatrix();
-				glColor3d(1.0, 0.5, 0);
-				glTranslated(getPosition().getX(), getPosition().getY(), getPosition().getZ());
+		//orange
+		glPushMatrix();
+		//glColor3d(1.0, 0.5, 0);
+		defineMaterial(0.1,0.05, 0, 1.00,	//Ambient
+			0.1, 0.05, 0, 1.00,	//Diffuse
+			0.1, 0.05, 0, 1.00,	//Specular
+			0.00, 0.00, 0.00, 1.00,	//Emission
+			77);					//SHININESS
+		glColor3f(1.0, 0.5, 0);
+		glTranslated(getPosition().getX(), getPosition().getY(), getPosition().getZ());
 
-				glRotated(_turnAngle * 180 / PI, 1, 0, 0);
-				//if turnangle > 180...
-				glPushMatrix();
-					glColor3d(0, 1, 0);
-					glTranslated(rand() % 1 + 0, rand() % 1 + 0, .5);
-					glScaled(.1, .5, .25);
-					glutSolidCube(1);
-				glPopMatrix();
-				glColor3d(1.0, 0.5, 0);
-				glutSolidSphere(.5, numberRings, numberRings);
-			glPopMatrix();
+		glRotated(_turnAngle * 180 / PI, 1, 0, 0);
+		//if turnangle > 180...
+		glPushMatrix();
+		glColor3d(0, 1, 0);
+		glTranslated(rand() % 1 + 0, rand() % 1 + 0, .5);
+		glScaled(.1, .5, .25);
+		glutSolidCube(1);
+		glPopMatrix();
+		glColor3d(1.0, 0.5, 0);
+		glutSolidSphere(.5, numberRings, numberRings);
+		glPopMatrix();
 		//}
 	}
 
-	void update(double delta_t,Car * myCar) {
+
+	void update(double delta_t, Car * myCar) {
 		if (myCar->HasColision(this)) {
-			//fazer update à posicao do carro APENAS
+			//fazer update Ã  posicao do carro APENAS
 			//NADA DE CRIAR DELETAR COISAS
 			myCar->setPosition(0, -6.5, 0);
 		}
@@ -69,7 +76,7 @@ public:
 			setCounter(delta_t + rand() % 20 + 2);
 			setLost(-1);
 		}
-		setTurnAngle(getTurnAngle()+ 0.05);
+		setTurnAngle(getTurnAngle() + 0.05);
 		setPosition(getPosition() + getSpeed() * delta_t);
 	}
 };
