fe045f1b25289bb4ac82d545e95d5367e410c84e
jessicaar <jessica_a.r@hotmail.com>
2015-11-22 16:33:36 +0000

    Pause

---
+++ ConsoleApplication1/Pause.h
@@ -0,0 +1,37 @@
+#ifndef __PAUSE__
+#define __PAUSE__
+
+#include "StaticObject.h"
+#include "Vector3.h"
+#include "GL/glut.h"
+
+
+class Pause : public StaticObject {
+
+public:
+	inline Pause() {}
+	inline ~Pause() {}
+	void draw(int camera, double x, double y, double z) {
+		defineMaterial(0.1, 0.1, 0.1, 1.00,	//Ambient
+			0.1, 0.1, 0.1, 1.00,	//Diffuse
+			0.1, 0.1, 0.1, 1.00,	//Specular
+			0, 0, 0, 0.5,	//Emission
+			77);					//SHININESS
+		glColor3f(0, 0, 0);
+		glPushMatrix();
+		
+
+		if (camera == 2) {
+			glTranslated(x, y, z); //1.5?
+			glScaled(2, 2, 2);
+			glutSolidCube(1); 
+			glPopMatrix();
+			return;
+		}
+		if (camera == 1) glRotated(30, 1, 0, 0);
+		glScaled(10, 4, 4);
+		glutSolidCube(1); 
+		glPopMatrix();
+	}
+};
+#endif
